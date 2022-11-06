9c76af37472ed8c755fa696284c15b25773b9a73
meriglmar <meriglmar@alum.us.es>
2022-11-06 18:27:29 +0100

    Entidad, repositorio, controlador y servicio de Jugador creados

--- src/main/java/org/springframework/samples/petclinic/jugador/JugadorRepository.java
+++ src/main/java/org/springframework/samples/petclinic/jugador/JugadorRepository.java
@@ -1,23 +1,21 @@
 package org.springframework.samples.petclinic.jugador;
 
-import java.util.List;
-
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JugadorRepository extends CrudRepository<Jugador, Integer> {
 
 
-public interface JugadorRepository extends Repository<Jugador, Integer>{
-    void save(Jugador jugador) throws DataAccessException;
-
-
-    //@Query("SELECT jugador FROM Jugador WHERE jugador.nombreUsuario LIKE :nombreUsuario%")
-	//public Collection<Jugador> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
-
-	@Query("SELECT jugador FROM Jugador jugador WHERE jugador.id =:id")
-	public Jugador findById(@Param("id") int id);
-
+//   @Query("SELECT jugador FROM Jugador jugador WHERE jugador.id =:id")
+//	public Jugador findById(@Param("id") int id);
+//
+//	@Query("SELECT jugador FROM Jugador jugador")
+//	public List<Jugador> findAll();
+	
 	@Query("SELECT jugador FROM Jugador jugador")
-	public List<Jugador> findAll();
+	Jugador findJugador();
 }
+
+
