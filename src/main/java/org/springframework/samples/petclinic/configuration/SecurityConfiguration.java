9703839a1f6a159b093d10d717c42a01129f58ed
meriglmar <meriglmar@alum.us.es>
2022-11-14 13:56:52 +0100

    Cambiar readme.md al juego de Buscaminas

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/session/**").permitAll()
         
 				.antMatchers("/players/new/**").permitAll()
-				.antMatchers("/players/myprofile/**").permitAll()
+				.antMatchers("/players/myprofile/**").hasAnyAuthority("player")
 				//.antMatchers("/myprofile/{username}/edit/**").hasAnyAuthority("player")
 				.antMatchers("/players/edit/**").hasAnyAuthority("player")
 				.antMatchers("/players/list/**").hasAnyAuthority("admin")
