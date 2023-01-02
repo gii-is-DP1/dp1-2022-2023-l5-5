eecf66ae154b85fc425097fa05ab687d2b46ec7f
meriglmar <meriglmar@alum.us.es>
2023-01-03 00:39:54 +0100

    Quitado anotación

--- src/main/java/org/springframework/samples/petclinic/MinesweeperApplication.java
+++ src/main/java/org/springframework/samples/petclinic/MinesweeperApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
 @SpringBootApplication()
 public class MinesweeperApplication {
 	
